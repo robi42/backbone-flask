cb06a48ecdbd021efffa1255f0d32e031cfdf6b7
Robert Thurnher <r.thurnher@gmail.com>
2011-04-17 04:10:38 +0200

    Some cleanup.

--- app.py
+++ app.py
@@ -7,7 +7,29 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
-@app.route('/todos')
+@app.route('/todos', methods=['GET', 'POST'])
+def create_or_retrieve_todos():
+    if request.method == 'POST':
+        return create_todo()
+    else:
+        return retrieve_todos()
+
+@app.route('/todos/<int:id>', methods=['PUT', 'DELETE'])
+def update_or_delete_todo(id):
+    if request.method == 'PUT':
+        return update_todo(id)
+    else:
+        return delete_todo(id)
+
+def create_todo():
+    data = json.loads(request.data)
+    todo = Todo(content=data['content'], done=data['done'], order=data['order'])
+    todo.put()
+    return jsonify(id=int(todo.key().id()),
+                   content=todo.content,
+                   done=todo.done,
+                   order=todo.order)
+
 def retrieve_todos():
     content = json.dumps([dict(id=todo.key().id(),
                                content=todo.content,
@@ -17,35 +39,15 @@ def retrieve_todos():
     response.mimetype = 'application/json'
     return response
 
-@app.route('/todos', methods=['POST'])
-def create_todo():
-    data = json.loads(request.data)
-    todo = Todo(content=data['content'], done=data['done'], order=data['order'])
-    todo.put()
-    response = make_response(jsonify(id=int(todo.key().id()),
-                                     content=todo.content,
-                                     done=todo.done,
-                                     order=todo.order))
-    response.mimetype = 'application/json'
-    return response
-
-@app.route('/todos/<int:id>', methods=['PUT'])
 def update_todo(id):
     todo = Todo.get_by_id(id)
     data = json.loads(request.data)
     todo.content = data['content']
     todo.done = data['done']
     todo.save()
-    response = make_response(jsonify(id=id,
-                                     content=todo.content,
-                                     done=todo.done))
-    response.mimetype = 'application/json'
-    return response
+    return jsonify(id=id, content=todo.content, done=todo.done)
 
-@app.route('/todos/<int:id>', methods=['DELETE'])
 def delete_todo(id):
     todo = Todo.get_by_id(id)
     todo.delete()
-    response = make_response()
-    response.mimetype = 'application/json'
-    return response
+    return jsonify()
