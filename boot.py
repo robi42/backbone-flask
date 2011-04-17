eeb3c52b1abd792d0d35a54872f43411f68b2510
Robert Thurnher <r.thurnher@gmail.com>
2011-04-17 03:47:40 +0200

    Add GAE stats recording middleware wrapping.

--- boot.py
+++ boot.py
@@ -4,8 +4,9 @@ root_dir = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, os.path.join(root_dir, 'lib'))
 
 from google.appengine.ext.webapp.util import run_wsgi_app
+from google.appengine.ext.appstats import recording
 from app import app
 
 if __name__ == '__main__':
-    run_wsgi_app(app)
+    run_wsgi_app(recording.appstats_wsgi_middleware(app))
   
